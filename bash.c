a2fc5fa0392d2b855bb2bb9e34a0270dd4ef43cb
Amber <amsmickler1@gmail.com>
2018-01-18 12:46:32 -0500

    Add files via upload

--- bash.c
+++ bash.c
@@ -4,3 +4,20 @@
 
 
 //TODO: the whole thing :D
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+  char instr[256];                          // user input will never be longer than 255 chars
+  char *user = getenv("USER");
+  char *pwd = getenv("PWD");
+
+  do {
+    printf("%s::%s$ ", user, pwd);          // shell header
+
+  } while(strncmp(instr, "exit", 4) != 0);  // run until exit command
+
+
+}
--- makefile
+++ makefile
@@ -1,1 +1,6 @@
-This is where the magic happens ;)
+# This is where the magic happens ;D
+
+myBash: bash.c
+	gcc -o myBash bash.c
+
+# What other flags are needed, if any?

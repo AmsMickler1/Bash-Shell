d27b470b8c70c3f7c2506331b07f7e48b6a4f523
Amber <amsmickler1@gmail.com>
2018-02-05 16:49:43 -0500

    Input redirection works and added echo

--- bash.c
+++ bash.c
@@ -2,8 +2,8 @@
 // COP4610 Intro to Operating Systems
 // Amber Mickler and Benjamin Hybart
 
-//DONE: Parse splits line into arguments, expand env vars, output redirection, execution (mostly)
-//TODO: resolve pathfiles, input redirection, background, pipes
+//DONE: Parse splits line into arguments, expand env vars, input & output redirection, execution (mostly)
+//TODO: resolve pathfiles, background, pipes
 
 #include <stdio.h>
 #include <string.h>
@@ -105,23 +105,42 @@ void parse(char* line) {
 
 
     /* BUILT INS */
-    if (strncmp(args[0], "exit", 4) == 0) {
+    if (strcmp(args[0], "exit") == 0) {
         printf("Exiting shell...\n");
         exit(0);
     }
-    else if (strncmp(args[0], "cd", 2) == 0) {
-        //input += 2;     // gets rid of "cd"
-        //char* rpath = resolve(input);
-        //if ( valid directory )
-        //    setenv("PWD", rpath);
+    else if (strcmp(args[0], "cd") == 0) {
+        if (index > 3)                                  // "cd" "path" "NULL"
+            printf("Error: Too many arguments.\n");
+        else {
+            //char* rpath = resolve(args[1]);
+            //if ( valid directory )
+            //    setenv("PWD", rpath);
+        }
     }
-    else if (strncmp(args[0], "echo", 4) == 0) {
+    else if (strcmp(args[0], "echo") == 0) {
+        int invalid = 0;
+        for (i=1; i<index-1; i++) {
+            if (args[i][0] == '$') {
+                if (expand(args[i]) == NULL)
+                    invalid = 1;
+            }
+        }
+        if (!invalid) {
+            for (i=1; i<index-1; i++) {
+                if (args[i][0] == '$')
+                    printf(expand(args[i]));
+                else
+                    printf(args[i]);
+                printf(" ");
+            }
+        printf("\n");
+        }
+    }
+    else if (strcmp(args[0], "etime") == 0) {
 
     }
-    else if (strncmp(args[0], "etime", 5) == 0) {
-
-    }
-    else if (strncmp(args[0], "io", 2) == 0) {
+    else if (strcmp(args[0], "io") == 0) {
 
     }
     else {
@@ -141,6 +160,8 @@ void parse(char* line) {
 // When executing run a for loop to check for $ then call this
 char* expand(char* envVar) {
     envVar++;                        // Drops leading $
+    if (getenv(envVar) == NULL)
+        printf("%s: Undefined Variable.\n", envVar);
     return getenv(envVar);
 }
 
@@ -248,6 +269,75 @@ void execute(char** cmd/*, char* path*/) {
     int status;
     int count = 0;
 
+    int inredir = 0;    // input redirect flag
+    int oredir = 0;     // output redirect flag
+    int pipe = 0;       // pipe flag
+    int background = 0; // background flag
+    int error = 0;      // error flag
+
+    char* input = NULL;        // input redirection filepath
+    char* output = NULL;       // output redirection filepath
+
+    int i;
+    for (i=0;i<256;i++) {
+        count++;
+        // If path can be resolved, resolve path and update cmd array
+
+        //char* temp = resolve(cmd[i]);
+        //if (temp != NULL) {
+        //    strncpy(cmd[i],temp,strlen(temp));
+        //}
+
+        // if is isSpecial
+        if (isSpecial(cmd[i][0])) {
+            // Check Syntax
+            if (cmd[i][0] != '&' && (i == 0 || strcmp(cmd[i+1],"NULL") == 0)) {
+                printf("Error: Invalid syntax.\n");
+                error = 1;
+            }
+            // set background flag
+            if (cmd[i][0] == '&' && i != 0)
+                background = 1;
+            else {
+                // set redirect flag input
+                if (cmd[i][0] == '<') {
+                    inredir = i;
+                    input = resolve(cmd[i+1]);
+                }
+                // set redirect flag output
+                if (cmd[i][0] == '>') {
+                    oredir = i;
+                    output = resolve(cmd[i+1]);
+                }
+            }
+            // set pipe flag here, this will probably need more to it
+            if (cmd[i][0] == '|') {
+                pipe = 1;
+            }
+
+        } // end of IsSpecial() check
+        if (cmd[i][0] == '$') {
+            if (expand(cmd[i]) != NULL)
+                strncpy(cmd[i],expand(cmd[i]), 255);
+        }
+        if (strcmp(cmd[i],"NULL") == 0) {
+            cmd[i] = NULL;
+            break;          //NULL is last arg, so no need to keep looping
+        }
+    } // end of FOR loop
+    // Put null after first redirect character to mark end of command and start of redirection
+    if (inredir != 0 && oredir != 0) {
+        if (inredir < oredir)
+            cmd[inredir] = NULL;
+        else
+            cmd[oredir] = NULL;
+    }
+    else if (inredir != 0 && oredir == 0)
+        cmd[inredir] = NULL;
+    else if (oredir != 0 && inredir == 0)
+        cmd[oredir] = NULL;
+
+
     pid_t pid = fork();
     if (pid == -1) {
         //Error
@@ -255,73 +345,23 @@ void execute(char** cmd/*, char* path*/) {
     }
     else if (pid == 0) {
         //Child
-        int inredir = 0;    // input redirect flag
-        int oredir = 0;     // output redirect flag
-        int pipe = 0;       // pipe flag
-        int background = 0; // background flag
-
-        char* input = NULL;        // input redirection filepath
-        char* output = NULL;       // output redirection filepath
-
-        int i;
-        for (i=0;i<256;i++) {
-            count++;
-            // If path can be resolved, resolve path and update cmd array
-
-            //char* temp = resolve(cmd[i]);
-            //if (temp != NULL) {
-            //    strncpy(cmd[i],temp,strlen(temp));
-            //}
-
-            // if is isSpecial
-            if (isSpecial(cmd[i][0])) {
-                // Check Syntax
-                if (cmd[i][0] != '&' && (i == 0 || strcmp(cmd[i+1],"NULL") == 0)) {
-                    printf("Error: Invalid syntax.\n");
-                    exit(1);
-                }
-                // set background flag
-                if (cmd[i][0] == '&' && i != 0)
-                    background = 1;
-                else {
-                    // set redirect flag input
-                    if (cmd[i][0] == '<') {
-                        inredir = 1;
-                        input = resolve(cmd[i+1]);
-                    }
-                    // set redirect flag output
-                    if (cmd[i][0] == '>') {
-                        oredir = 1;
-                        output = resolve(cmd[i+1]);
-                    }
-                    cmd[i] = NULL;  // NULL terminates command, otherwise execv will try to execute > as a param
-                    break;
-                }
-                // set pipe flag here, this will probably need more to it
-                if (cmd[i][0] == '|') {
-                    pipe = 1;
-                }
-
-            } // end of IsSpecial() check
-
-            if (cmd[i][0] == '$')
-                strncpy(cmd[i],expand(cmd[i]), 255);
-            if (strcmp(cmd[i],"NULL") == 0) {
-                cmd[i] = NULL;
-                break;          //NULL is last arg, so no need to keep looping
-            }
-        } // end of FOR loop
 
         // print new args and redirects for debugging
         printf("New args: \'%s\'", cmd[0]);
         for (i=1;i<count;i++) {
             printf(", \'%s\'", cmd[i]);
         }
-        printf("\nRedirect to/from: %s/%s\n", output, input);
-
+        printf("\n");
+        //printf("\nRedirect to/from: %s -/- %s\n", output, input);
+        if (error)
+            exit(1);
         // if input redirects
         if (inredir) {
             int fd = open(input, O_RDONLY);
+            if (fd == -1) {
+                printf("Error: File does not exist or is not a file.\n");
+                exit(1);
+            }
             close(STDIN_FILENO);
             dup(fd);
             close(fd);
@@ -329,7 +369,7 @@ void execute(char** cmd/*, char* path*/) {
 
         // if output redirects
         if (oredir) {
-            int fd = open(output, O_CREAT|O_WRONLY, 0600);
+            int fd = open(output, O_CREAT|O_WRONLY|O_TRUNC, 0600);
             close(STDOUT_FILENO);
             dup(fd);
             close(fd);
